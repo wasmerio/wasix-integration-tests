b8865f049389eca486e13aa62a4cc47286e38f2e
Christoph Herzog <chris@theduke.at>
2022-11-27 01:27:25 +0100

    Rust ureq example: add some more logging

--- rust/ureq-http-client/src/main.rs
+++ rust/ureq-http-client/src/main.rs
@@ -1,15 +1,15 @@
-fn main() -> Result<(), ureq::Error> {
+fn main() {
     let url = std::env::args()
         .skip(1)
         .next()
         .unwrap_or_else(|| "http://example.com".to_string());
 
-    eprintln!("Fetching url: {url}");
+    println!("Fetching url: {url}");
 
-    let body: String = ureq::get(&url)
+    let res = ureq::get(&url)
         // .set("Example-Header", "header value")
-        .call()?
-        .into_string()?;
-    eprintln!("BODY:\n----------\n{body}\n---------\n");
-    Ok(())
+        .call().unwrap();
+    println!("got response");
+    let body: String = res.into_string().unwrap();
+    println!("BODY:\n----------\n{body}\n---------\n");
 }
