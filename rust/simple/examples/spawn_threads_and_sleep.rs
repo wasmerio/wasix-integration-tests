4526896d80ce9dc0c155de5a6c340acf6d994766
Christoph Herzog <chris@theduke.at>
2023-01-23 12:37:51 +0100

    rust: Add thread spawn example

---
+++ rust/simple/examples/spawn_threads_and_sleep.rs
@@ -0,0 +1,28 @@
+/// Spawn a number of threads that sleep for inifinity.
+
+use std::io::BufRead;
+
+fn main() {
+    let count: usize = if let Some(v) = std::env::args().skip(1).next() {
+        v.parse()
+            .expect("first argument must be the number of threads")
+    } else {
+        99
+    };
+
+    eprintln!("Spawning {} threads", count);
+
+    let sleeptime = std::time::Duration::from_secs(1000000);
+
+    for index in 1..=count {
+        std::thread::spawn(move || {
+            println!("thread {} started", index);
+            std::thread::sleep(sleeptime);
+        });
+    }
+
+    eprintln!("All threads spawned");
+    eprintln!("Press enter to exit");
+
+    std::io::stdin().lock().lines().next();
+}
